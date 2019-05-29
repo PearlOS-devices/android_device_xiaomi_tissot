46a94f9fd258a488698b25c683fdeca22fdda3f8
himanshu <hnaamdev41@gmail.com>
2019-05-29 13:14:53 +0530

    Added Official Tag

--- pearl_tissot.mk
+++ pearl_tissot.mk
@@ -41,5 +41,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
 
+#Boot Animation res
 TARGET_BOOT_ANIMATION_RES := 1080
 
+CPU_MODEL := "Snapdragon_625"
+PEARL_BUILD_TYPE := "OFFICIAL"
+PEARL_MAINTAINER := "Himanshu Naamdev"
